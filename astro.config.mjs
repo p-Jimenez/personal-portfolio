82e06ae6d9d435bc4dbbf7efbb288da2c73f6e1d
p-Jimenez <pablo.jimg@gmail.com>
2024-02-08 21:30:22 +0100

    Temporal output server

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,15 @@
 import { SITE_URL } from "./src/data/config";
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 import robotsTxt from "astro-robots-txt";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 
-import react from "@astrojs/react";
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap(), robotsTxt(), react()],
-  output: "static",
+  // output: "static",
   site: SITE_URL,
   server: {
     port: 3000
